739a3a14cf4ca3faabb2a937ba622c495f409a7a
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-07-30 17:44:29 +0700

    init posttype

---
+++ inc/Post/Temple.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Summary
+ * Description.
+ *
+ * @since  0.9.0
+ * @package
+ * @subpackage
+ * @author nguyenvanduocit
+ */
+
+namespace BuddhistDirectory\Post;
+
+
+class Temple {
+
+}
