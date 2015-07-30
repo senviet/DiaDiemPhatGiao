34d9168d6e9e979c8b94a7b4bf848fcdbef0512e
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-07-30 17:16:46 +0700

    split header, content and footer

--- footer.php
+++ footer.php
@@ -1,10 +1,20 @@
-<?php
-/**
- * Summary
- * Description.
- *
- * @since  0.9.0
- * @package
- * @subpackage
- * @author nguyenvanduocit
- */
+<footer class="site-footer">
+	<div class="footer-description-container">
+		<div class="container">
+			<p class="footer-description">Launchpad provides you with the technology, online resources, expertise and community to launch and scale your app.</p>
+		</div>
+	</div>
+	<div class="footer-apply-container">
+		<div class="container">
+			<a href="https://developers.google.com/startups/apply" class="btn-apply">APPLY NOW</a>
+			<a href="/startups" class="footer-launch-link">
+				<img src="images/logo-launch-footer.svg" alt="Google Developers Startup Launch">
+			</a>
+		</div>
+	</div>
+</footer>
+<input class="gc-analytics" type="hidden" value="UA-24532603-1"/>
+<input class="gc-analytics" type="hidden" value="UA-49880327-1"/>
+<script src="/_static/js/managed/analytics.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,105 +1,6 @@
-<!doctype html><!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]--><!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8"> <![endif]--><!--[if IE 8]>
-<html class="no-js lt-ie9"> <![endif]--><!--[if gt IE 8]><!-->
-<html class="no-js"> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Google Launchpad</title>
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<link rel="stylesheet" href="<?php _e(get_template_directory_uri()) ?>/css/main.css">
-
-	<link rel="apple-touch-icon" sizes="57x57" href="/images/favicons/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/images/favicons/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/images/favicons/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/images/favicons/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/images/favicons/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/images/favicons/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="/images/favicons/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="/images/favicons/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="/images/favicons/manifest.json">
-	<link rel="shortcut icon" href="images/favicon.gif">
-	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="msapplication-TileImage" content="/images/favicons/mstile-144x144.png">
-	<meta name="msapplication-config" content="/images/favicons/browserconfig.xml">
-	<meta name="google-site-verification" content="582y1Zjr-j5pp4gdVfTEjsWtMQi1QUEb5FMbJN4hEus"/>
-	<meta name="theme-color" content="#ffffff">
-
-
-</head>
-<body>
-<section class="header">
-	<div class="header-logo-container">
-		<div class="header-inner-container logo">
-			<a href="/startups" class="header-logo"><img src="images/logo-launch.svg" alt="Google Launch"></a>
-
-			<div class="apply-container">
-				<div class="inner">
-					<a href="https://developers.google.com/startups/apply" target="_new" class="apply-link btn-apply">APPLY NOW</a>
-					<a href="http://g.co/launchplus" class="apply-link btn-gplus" target="_new"><img src="images/logo-gplus-red.svg" alt="Google Launch on Google+"></a>
-					<a href="https://www.youtube.com/channel/UCorTyjVGM-PV5CCKbosONow" target="_new" class="apply-link btn-yt"><img src="images/logo-yt-red.svg" alt="Google Launch on Youtube"></a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="header-dropdown-container">
-		<div class="header-inner-container dropdown">
-			<input type="checkbox" id="header-drop-checkbox">
-			<label for="header-drop-checkbox" class="header-drop-btn"></label>
-			<nav role="navigation" class="header-dropdown">
-				<ul class="header-dropdown-items">
-					<li class="header-dropdown-item">
-						<a href="about.html"><span class="icon"></span>
-
-							<div class="text">
-								<h3>About</h3>
-
-								<p>Learn how Launchpad can help you.</p>
-							</div>
-						</a>
-					</li>
-					<li class="header-dropdown-item">
-						<a href="events.html"><span class="icon"></span>
-
-							<div class="text">
-								<h3>Events</h3>
-
-								<p>Join us at an event near you.</p>
-							</div>
-						</a>
-					</li>
-					<li class="header-dropdown-item">
-						<a href="resources.html"><span class="icon"></span>
-
-							<div class="text">
-								<h3>Resources</h3>
-
-								<p>Start scaling now.</p>
-							</div>
-						</a>
-					</li>
-					<li class="header-dropdown-item">
-						<a href="spotlight.html"><span class="icon"></span>
-
-							<div class="text">
-								<h3>Spotlight</h3>
-
-								<p>Tips and tricks from leading startups. </p>
-							</div>
-						</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-</section>
+<?php
+get_header();
+?>
 <section class="intro">
 	<div class="left column">
 		<div class="intro-image-container">
@@ -407,24 +308,6 @@
 </section>
 
 <div class="photo-strip" style="background-image: url('images/photo-strip-home.jpg');"></div>
-
-<footer class="site-footer">
-	<div class="footer-description-container">
-		<div class="container">
-			<p class="footer-description">Launchpad provides you with the technology, online resources, expertise and community to launch and scale your app.</p>
-		</div>
-	</div>
-	<div class="footer-apply-container">
-		<div class="container">
-			<a href="https://developers.google.com/startups/apply" class="btn-apply">APPLY NOW</a>
-			<a href="/startups" class="footer-launch-link">
-				<img src="images/logo-launch-footer.svg" alt="Google Developers Startup Launch">
-			</a>
-		</div>
-	</div>
-</footer>
-<input class="gc-analytics" type="hidden" value="UA-24532603-1"/>
-<input class="gc-analytics" type="hidden" value="UA-49880327-1"/>
-<script src="/_static/js/managed/analytics.js"></script>
-</body>
-</html>
+<?php
+get_footer();
+?>
